7a5d0f079aedcc4a40bfda0e2ed584ab6c4404f7
Daniil Avtushko <adanilcoder@gmail.com>
2023-05-31 23:32:02 +0300

    refactor main

--- cmd/qr/main.go
+++ cmd/qr/main.go
@@ -1,41 +1,18 @@
 package main
 
 import (
-	"flag"
 	"fmt"
-	"log"
 
 	"github.com/psxzz/go-qr/pkg/qr"
 )
 
-var corrFlag = flag.String("corr", "L", "Correction level")
-var corr qr.Correction
-
 func main() {
-	flag.Parse()
-
-	switch *corrFlag {
-	case "L":
-		corr = qr.L
-	case "M":
-		corr = qr.M
-	case "Q":
-		corr = qr.Q
-	case "H":
-		corr = qr.H
-	}
-
-	encoder := qr.NewEncoder(
-		qr.WithCorrectionLevel(corr),
-		qr.WithVersionRange(0, 40),
-	)
+	encoder := qr.NewEncoder(qr.WithCorrectionLevel(qr.M))
 
 	code, err := encoder.Encode("https://github.com/psxzz/go-qr")
 
 	if err != nil {
-		log.Fatalf("encoder: %v", err)
+		panic(err)
 	}
-
 	fmt.Printf("code: %v\n", code)
-
 }
